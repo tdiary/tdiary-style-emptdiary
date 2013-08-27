95da2fceb370dda5f8cdf3db4d5f38e296bfe35a
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-08-27 19:59:56 +0900

    bundle gem tdiary-style-emptdiary -t

---
+++ lib/tdiary/style/emptdiary.rb
@@ -0,0 +1,9 @@
+require "tdiary/style/emptdiary/version"
+
+module Tdiary
+  module Style
+    module Emptdiary
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tdiary/style/emptdiary/version.rb
@@ -0,0 +1,7 @@
+module Tdiary
+  module Style
+    module Emptdiary
+      VERSION = "0.0.1"
+    end
+  end
+end
