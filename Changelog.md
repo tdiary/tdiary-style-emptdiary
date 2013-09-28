91fda7f9ba22555edf5e02f888d524934e77ebbf
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-28 13:25:46 +0900

    added Changelog

---
+++ Changelog.md
@@ -0,0 +1,13 @@
+## Mon Feb 17, 2003 zunda <zunda at freeshell.org>
+
+  * copied from zunda_style.rb, with which I used for preliminary tests.
+
+## Wed Feb 26, 2003 zunda <zunda at freeshell.org>
+
+ * TDiary::Emptdiary::String class to handle strings better
+	(Thanks to Mitsuru Shimamura <mitsuru at diana.dti.ne.jp> for the
+	error report)
+
+## Wed Dec 22, 2004 zunda <zunda at freeshell.org>
+
+ * override body_to_html
--- lib/tdiary/style/emptdiary.rb
+++ lib/tdiary/style/emptdiary.rb
@@ -26,16 +26,6 @@ tdiary_style.rb and slightly edited as follows:
 * Regexp is chanegd from ^ to \A
 
 =end
-=begin ChangeLog
-* Mon Feb 17, 2003 zunda <zunda at freeshell.org>
-- copied from zunda_style.rb, with which I used for preliminary tests.
-* Wed Feb 26, 2003 zunda <zunda at freeshell.org>
-- TDiary::Emptdiary::String class to handle strings better
-	(Thanks to Mitsuru Shimamura <mitsuru at diana.dti.ne.jp> for the
-	error report)
-* Wed Dec 22, 2004 zunda <zunda at freeshell.org>
-- override body_to_html
-=end
  
 require 'tdiary/style/tdiary'
 
